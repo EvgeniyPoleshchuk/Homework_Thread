48ecdb8580f89bec1e5ff4e529b334706043e3cd
Evgeniy Poleshchuk <2x2great@gmail.com>
2023-06-02 16:36:29 +0300

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,18 @@
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
         long startTs = System.currentTimeMillis(); // start time
-        List<Thread> threads = new ArrayList<>();
+        List<Future<Integer>> futures = new ArrayList<>();
+        ExecutorService threadPool = Executors.newFixedThreadPool(4);
         for (String text : texts) {
-            Runnable logic = () -> {
+            Callable<Integer> callable = () -> {
                 int maxSize = 0;
                 for (int i = 0; i < text.length(); i++) {
                     for (int j = 0; j < text.length(); j++) {
@@ -30,15 +32,19 @@ public class Main {
                     }
                 }
                 System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                return maxSize;
             };
-            Thread thread = new Thread(logic);
-            thread.start();
-            threads.add(thread);
+            Future<Integer> future = threadPool.submit(callable);
+            futures.add(future);
         }
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
-            System.out.println("Поток завершен : " + thread.getName());
+        int max = 0;
+        for (Future<Integer> future : futures) {
+            if(max < future.get()){
+              max = future.get();
+          }
         }
+        System.out.println("Максимальноe значение : " + max);
+
         long endTs = System.currentTimeMillis(); // end time
         System.out.println("Time: " + (endTs - startTs) + "ms");
     }
